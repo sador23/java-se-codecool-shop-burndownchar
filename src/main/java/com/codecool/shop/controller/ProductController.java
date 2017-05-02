3e264393abd2f5c196dd007b60d0f145ceb88048
Varga Tamás <varga.tamas19860808@gmail.com>
2017-05-02 14:05:40 +0200

    some tests are done

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -14,7 +14,9 @@ import spark.Request;
 import spark.Response;
 import spark.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ProductController {
@@ -22,8 +24,14 @@ public class ProductController {
     public static ModelAndView renderProducts(Request req, Response res) {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+
+        List<Supplier> suppliers = supplierDataStore.getAll();
+        List<ProductCategory> productCategories = productCategoryDataStore.getAll();
 
         Map params = new HashMap<>();
+        params.put("suppliers",suppliers);
+        params.put("categories",productCategories);
         params.put("category", productCategoryDataStore.find(1));
         params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         return new ModelAndView(params, "product/index");
