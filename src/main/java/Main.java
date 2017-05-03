6b9b722b363ba5c52ebcd5072a89b3a0522e17f9
Varga Tamás <varga.tamas19860808@gmail.com>
2017-05-03 14:30:19 +0200

    getting the checkout data into backend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,18 +26,16 @@ public class Main {
 
         // Always start with more specific routes
         get("/cart", (Request req, Response res) -> {
-            return new ThymeleafTemplateEngine().render( ProductController.renderCart(req, res) );
+            return new ThymeleafTemplateEngine().render(ProductController.renderCart(req, res));
         });
-
-
 
 
         get("/cart/edit/:id", (Request req, Response res) -> {
-            return new ThymeleafTemplateEngine().render( ProductController.renderCart(req, res) );
+            return new ThymeleafTemplateEngine().render(ProductController.renderCart(req, res));
         });
 
         get("/cart/delete/:id", (Request req, Response res) -> {
-            return new ThymeleafTemplateEngine().render( ProductController.deleteItem(req, res) );
+            return new ThymeleafTemplateEngine().render(ProductController.deleteItem(req, res));
         });
 
 
@@ -45,13 +43,13 @@ public class Main {
         get("/", ProductController::renderProducts, new ThymeleafTemplateEngine());
         // Equivalent with above
         get("/index", (Request req, Response res) -> {
-           return new ThymeleafTemplateEngine().render( ProductController.renderProducts(req, res) );
+            return new ThymeleafTemplateEngine().render(ProductController.renderProducts(req, res));
         });
 
 
-        get ("/:id", (req, res) -> {
+        get("/:id", (req, res) -> {
 //            res.redirect("/");
-            return new ThymeleafTemplateEngine().render( OrderController.addProductToOrder(req, res) );
+            return new ThymeleafTemplateEngine().render(OrderController.addProductToOrder(req, res));
         });
 
         get("/categories/:id", ProductController::renderProductsByCategory, new ThymeleafTemplateEngine());
@@ -59,6 +57,17 @@ public class Main {
         get("/suppliers/:id", ProductController::renderProductsBySupplier, new ThymeleafTemplateEngine());
 
         get("/order/checkout", CheckoutController::renderCheckoutForm, new ThymeleafTemplateEngine());
+
+        post("order/checkout/done", (request, response) -> {
+            Person1 person = new Person1(request.queryParams("first_name"), request.queryParams("last_name"),
+                    request.queryParams("phone_number"), request.queryParams("email_address"),
+                    request.queryParams("shipping-country"), request.queryParams("shipping-city"),
+                    request.queryParams("shipping-zip-code"), request.queryParams("shipping-address"),
+                    request.queryParams("billing-country"), request.queryParams("billing-city"),
+                    request.queryParams("billing-zip_code"), request.queryParams("billing-address"));
+            return person;
+        });
+
 
         // Add this line to your project to enable the debug screen
         enableDebugScreen();
