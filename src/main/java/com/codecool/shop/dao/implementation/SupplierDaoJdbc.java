b3b133cb0e210ddb15fb88a8e266e859cd310651
Varga Tamás <varga.tamas19860808@gmail.com>
2017-05-17 11:49:36 +0200

    DatabaseBuilder file created

---
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
@@ -0,0 +1,22 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.SupplierDao;
+
+/**
+ * Created by tomi on 2017.05.17..
+ */
+//public class SupplierDaoJdbc implements SupplierDao {
+//
+//    private static SupplierDaoJdbc instance = null;
+//
+//    private SupplierDaoJdbc() {
+//    }
+//
+//    public static SupplierDaoJdbc getInstance(){
+//        if (instance == null) {
+//            instance = new SupplierDaoJdbc();
+//        }
+//        return instance;
+//    }
+//
+//}
