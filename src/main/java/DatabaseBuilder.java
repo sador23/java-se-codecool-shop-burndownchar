bd3065f9de0d4d195eff6d0a457d45ee07c9b509
Varga Tamás <varga.tamas19860808@gmail.com>
2017-05-17 17:25:54 +0200

    ProductCategoryDaoJdbc and SupplierDaoJdbc classes implemented

--- src/main/java/DatabaseBuilder.java
+++ src/main/java/DatabaseBuilder.java
@@ -1,6 +1,5 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import com.codecool.shop.dao.implementation.PropertiesReader;
+
 import java.sql.*;
 
 
@@ -8,31 +7,16 @@ import java.sql.*;
  * Created by tomi on 2017.05.17..
  */
 public class DatabaseBuilder {
-    public static String user;
-    public static String psw;
-    public static String database;
-    public static String db_url;
-    public static String JDBC_DRIVER = "jdbc:postgresql";
-
-    public static void readData() {
-        try {
-            Properties properties = new Properties();
-            InputStream inputStream = Main.class.getResourceAsStream("sql/connection.properties");
-            properties.load(inputStream);
-            database = properties.getProperty("database");
-            user = properties.getProperty("user");
-            db_url = properties.getProperty("url");
-            psw = properties.getProperty("password");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    public static PropertiesReader propertiesReader = new PropertiesReader();
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        readData();
+        propertiesReader.readData();
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://"+db_url+"/"+database,user,psw);
+                "jdbc:postgresql://"+propertiesReader.db_url+"/"
+                        +propertiesReader.database,
+                propertiesReader.user,
+                propertiesReader.psw);
         System.out.println("Establishing database connection...");
         System.out.println("Connecting to codecoolshop database...");
         System.out.println("Connected database successfully...");
---
+++ src/main/java/com/codecool/shop/dao/implementation/PropertiesReader.java
@@ -0,0 +1,29 @@
+package com.codecool.shop.dao.implementation;
+
+import java.io.*;
+import java.util.Properties;
+
+/**
+ * Created by tomi on 2017.05.17..
+ */
+public class PropertiesReader {
+    public static String user;
+    public static String psw;
+    public static String database;
+    public static String db_url;
+    public static String JDBC_DRIVER = "jdbc:postgresql";
+
+    public void readData() {
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("connection.properties");
+            properties.load(inputStream);
+            database = properties.getProperty("database");
+            user = properties.getProperty("user");
+            db_url = properties.getProperty("url");
+            psw = properties.getProperty("password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
